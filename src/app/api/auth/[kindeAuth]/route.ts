5c834565fbbe87ca664cf0c483af35715d9f8255
Ashher Ali <ashher868@gmail.com>
2023-12-17 20:54:29 +0530

    working

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,10 @@
+// @ts-ignore    <-- add this
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
-
+// @ts-ignore    <-- add this
 export async function GET(request: NextRequest, {params}: any) {
+	// @ts-ignore    <-- add this
 	const endpoint = params.kindeAuth;
 	return handleAuth(request, endpoint);
 }
+
