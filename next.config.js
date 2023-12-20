ad016e3403d39385b3c732f649958cd239f8ef39
Ashher Ali <ashher868@gmail.com>
2023-12-20 18:24:16 +0530

    Resolved conflicts from pull

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+  async redirects() {
+
+    return [
+      {
+        source: "/sign-in",
+        destination: '/api/auth/login',
+        permanent: true
+      },
+      {
+        source: "/sign-up",
+        destination: '/api/auth/register',  
+        permanent: true
+      },
+    ]
+
+  },
+
   webpack: (config, {buildId, dev, isServer, defaultLoaders, webppack}) => {
     config.resolve.alias.canvas = false;
 
