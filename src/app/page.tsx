9b84d40063a7ad7018ec82902c809ff8c8f69e9c
Ashher Ali <ashher868@gmail.com>
2023-12-22 18:54:07 +0530

    Thumbnail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,19 +10,17 @@ export default function Home() {
       <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-grey-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-grey-300 hover:bg-white/50">
           <p className="text-sm font-semibold text-grey-700">
-            Intellico is now public!
+          Intellico is live and open to all for seamless access and exploration!
           </p>
         </div>
 
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
-          Chat with your <span className="text-blue-600">documents</span> in
-          seconds.
+        AI-Powered Analysis for Your <span className="text-blue-600">Documents</span>
         </h1>
 
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg">
-          Intellico allows you to have conversations with any PDF document.
-          Simply upload your file and start asking questions right away.
-        </p>
+        Intellico simplifies document analysis, effortlessly examining research papers, resumes and other documents.It&apos;s as easy as uploading a PDF and querying AI!
+                </p>
 
         {/* <Link
           className={buttonVariants({
@@ -51,6 +49,7 @@ export default function Home() {
           className={buttonVariants({
             size: "lg",
             className: "mt-5",
+            variant: 'secondary',
           })}
           href={"/dashboard"}
           target="_blank"
@@ -69,7 +68,7 @@ export default function Home() {
                 clipPath:
                   "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
               }}
-              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#d03a3a] to-[#d50101] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             >
              
             </div>
